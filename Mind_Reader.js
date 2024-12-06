776118bf0fff22236539980b586485774f8c6205
Mp-Prince <prince.pc.6366@gmail.com>
2024-12-06 07:49:40 +0530

    My first and Simple Program

---
+++ Mind_Reader.js
@@ -0,0 +1,14 @@
+alert("Thank of a number ");
+
+alert("Duble the number \n means multiply the number with 2");
+
+let randomNumber = Math.floor(Math.random()*100);
+let add = "Add the number "+randomNumber;
+alert(add);
+alert("Divide the number in half");
+
+alert("Subtract the number which u was thanking in the Beggning ");
+
+result = "The Number In ur Mind is : "+randomNumber/2+" ";
+
+alert(result);
